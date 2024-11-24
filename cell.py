223f0934a909d7cae68933b0d40dc9411d6c901c
EAHowes <ethanhowes06@gmail.com>
2024-11-24 08:27:06 -0500

    started and finished cell class

--- cell.py
+++ cell.py
@@ -1,23 +1,42 @@
+import pygame
+import constants
 
 class Cell:
     def __init__(self, value, row, col, screen):
-        """Constructor
-        for the Cell class"""
-        pass
+        self.value = value
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.sketched_value = 0
+        self.selected = False
 
     def set_cell_value(self, value):
-        """Setter
-        for this cell’s value"""
-        pass
+        self.value = value
 
     def set_sketched_value(self, value):
-        """Setter
-        for this cell’s sketched value"""
-        pass
+        self.sketched_value = value
 
     def draw(self):
-        """Draws this cell, along with the value inside it.
-        If this cell has a nonzero value, that value is displayed.
-        Otherwise, no value is displayed in the cell.
-        The cell is outlined red if it is currently selected."""
-        pass
+        x = self.col * constants.SQUARE_SIZE
+        y = self.row * constants.SQUARE_SIZE
+
+        if self.selected:
+            #sets to red
+            color = (255, 0, 0)
+        else:
+            #sets to black
+            color = (0, 0, 0)
+            
+        pygame.draw.rect(self.screen, color, (x, y, constants.SQUARE_SIZE, constants.SQUARE_SIZE), 2)
+        font_size = pygame.font.Font(None, 40)
+
+
+        if self.value != 0:
+            text = font_size.render(str(self.value), True, (0, 0, 0))
+            text_rectangle = text.get_rect(center=(x + constants.SQUARE_SIZE // 2, y + constants.SQUARE_SIZE // 2))
+            self.screen.blit(text, text_rectangle)
+            
+        elif self.sketched_value != 0:
+            text = font_size.render(str(self.sketched_value), True, (128, 128, 128))
+            text_rectangle = text.get_rect(center=(x + constants.SQUARE_SIZE // 2, y + constants.SQUARE_SIZE // 2))
+            self.screen.blit(text, text_rectangle)
